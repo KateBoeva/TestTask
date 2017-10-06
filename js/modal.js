f71db1a9cd939b14e0b36fc00cf6575da9eea142
katemrrr <katrinb_08@mail.ru>
2017-10-06 21:43:04 +0300

    add fade animation

--- js/modal.js
+++ js/modal.js
@@ -6,11 +6,17 @@ var dark = document.getElementById('dark_back');
 var win = document.getElementById('modal');
 
 function openWindow() {
+    win.classList.remove('fadeOut');
+    win.classList.add('fadeIn');
     win.style.display = 'block';
     dark.style.display = 'block';
 }
 
 function closeWindow() {
-    win.style.display = 'none';
+    win.classList.remove('fadeIn');
+    win.classList.add('fadeOut');
+    setTimeout(function () {
+        win.style.display = 'none';
+    }, 500);
     dark.style.display = 'none';
 }
